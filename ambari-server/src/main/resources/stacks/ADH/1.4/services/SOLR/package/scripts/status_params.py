2ae985d34a387fd3c5100ec8ed92673dd8b9ba27
Alexander <Alexander@MacBook-Pro-Alexander.local>
2017-06-26 12:28:01 +0300

    Revert "AMBARI-16 Added Lucence Configuration"
    This reverts commit 9a345287734aacd09400848c6df4c45b48e1eb50.

--- ambari-server/src/main/resources/stacks/ADH/1.4/services/SOLR/package/scripts/status_params.py
+++ ambari-server/src/main/resources/stacks/ADH/1.4/services/SOLR/package/scripts/status_params.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
+from resource_management import *
 
-from resource_management.libraries.functions.format import format
-from resource_management.libraries.script.script import Script
-
+# config object that holds the status related configurations declared in the -env.xml file
 config = Script.get_config()
 
-map_solr_config = config['configurations']['solr-config-env']
-
-solr_config_port = map_solr_config['solr_config_port']
-solr_config_pid_dir = map_solr_config['solr_config_pid_dir']
-solr_config_pid_file = format('{solr_config_pid_dir}/solr-{solr_config_port}.pid')
+solr_piddir = config['configurations']['solr-env']['solr_pid_dir']
+solr_port = config['configurations']['solr-env']['solr.port']
+solr_pidfile = format("{solr_piddir}/solr-{solr_port}.pid")
