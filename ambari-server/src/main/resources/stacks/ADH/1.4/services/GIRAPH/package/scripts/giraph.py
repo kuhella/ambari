663028e679e782acee5a4664218f5daf3ab630bf
Anton Chevychalov <pub@mnu.pp.ru>
2017-07-07 17:50:34 +0300

    Import ADH 1.4 stack
    That based on branch-adh-1.4 commit
    33fcac29f389fff6165f577ebb84deb03ce4c73f

---
+++ ambari-server/src/main/resources/stacks/ADH/1.4/services/GIRAPH/package/scripts/giraph.py
@@ -0,0 +1,50 @@
+"""
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Ambari Agent
+
+"""
+
+import sys
+import os
+import logging
+from resource_management import *
+from resource_management.libraries.functions.check_process_status import check_process_status
+from resource_management.libraries.resources.hdfs_resource import HdfsResource
+from resource_management.core.resources.service import Service
+
+class Giraph(Script):
+  def install(self, env):
+    import params
+    env.set_params(params)
+    self.install_packages(env)
+
+  def configure(self, env, upgrade_type=None):
+    import params
+    env.set_params(params)
+
+  def stop(self, env):
+    return True
+
+  def start(self, env):
+    return True
+
+  def status(self, env):
+    return True
+
+if __name__ == "__main__":
+  Giraph().execute()
