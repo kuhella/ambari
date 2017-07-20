da97f87ec73d0b334f8ae62d5befd3dfaf6e646e
root <root@valery-13-qr79.c.arenadata-test.internal>
2017-07-20 09:31:11 +0000

    ADH-155 hue stop-start crush fix
    (cherry picked from commit 4c0fc13986e3440f0844832e102e5bcf3246bad9)

--- ambari-server/src/main/resources/stacks/ADH/1.4/services/HUE/package/scripts/setup_hue.py
+++ ambari-server/src/main/resources/stacks/ADH/1.4/services/HUE/package/scripts/setup_hue.py
@@ -9,17 +9,10 @@ def setup_hue():
   import status_params
   Logger.info("Configure Hue Service")
   # create the pid and log dir
-  Execute("mkdir -p --mode=0755 " + params.hue_log_dir + " " + params.hue_pid_dir) # NEW
-  Execute("chown " + params.hue_user + ":" + params.hue_group + " " + params.hue_log_dir + " " + params.hue_pid_dir) # NEW
-  Execute("ln -s /var/log/hue/ /usr/lib/hue/logs")
-  # CHANGE
-  # Directory([params.hue_log_dir, params.hue_pid_dir],
-  #      mode=0755,
-  #      cd_access='a',
-  #      owner=params.hue_user,
-  #      group=params.hue_group,
-  #      parents=True # CHANGE
-  #)
+  Execute("mkdir -p --mode=0755 " + params.hue_log_dir + " " + params.hue_pid_dir)
+  Execute("chown " + params.hue_user + ":" + params.hue_group + " " + params.hue_log_dir + " " + params.hue_pid_dir)
+  if not os.path.islink('/usr/lib/hue/logs/hue'):
+    Execute("ln -s /var/log/hue/ /usr/lib/hue/logs")
   File([params.hue_log_file, params.hue_server_pid_file],
     mode=0644,
     owner=params.hue_user,
@@ -28,7 +21,7 @@ def setup_hue():
   )
     
   Logger.info("Creating symlinks /usr/hdp/current/hadoop-client/lib/hue-plugins-3.11.0-SNAPSHOT.jar")
-  Link("{0}/desktop/libs/hadoop/java-lib/*".format(params.hue_dir),to = "/usr/lib") # CHANGE
+  Link("{0}/desktop/libs/hadoop/java-lib/*".format(params.hue_dir),to = "/usr/lib")
   Execute('find {0} -iname "*.sh" | xargs chmod +x'.format(params.service_packagedir))
   # Create a home directory for solr user on HDFS
   params.HdfsResource(params.hue_hdfs_home_dir,
