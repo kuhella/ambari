3739f3023aaa7013d10602f32e2cc386f3bdb3dd
Alex Ryndin <alexnryndin@gmail.com>
2017-08-31 14:14:19 +0000

    ADH-276 Added some setting to properly start/restart DataNode on Kerberized clluster.

--- ambari-server/src/main/resources/stacks/ADH/1.0/services/HDFS/package/scripts/params_linux.py
+++ ambari-server/src/main/resources/stacks/ADH/1.0/services/HDFS/package/scripts/params_linux.py
@@ -33,7 +33,7 @@ from resource_management.libraries.functions import get_klist_path
 from resource_management.libraries.functions import get_kinit_path
 from resource_management.libraries.script.script import Script
 from resource_management.libraries.resources.hdfs_resource import HdfsResource
-
+from resource_management.libraries.functions.version import format_stack_version
 from resource_management.libraries.functions.format_jvm_option import format_jvm_option
 from resource_management.libraries.functions.get_lzo_packages import get_lzo_packages
 from resource_management.libraries.functions.is_empty import is_empty
@@ -45,6 +45,10 @@ tmp_dir = Script.get_tmp_dir()
 stack_name = default("/hostLevelParams/stack_name", None)
 upgrade_direction = default("/commandParams/upgrade_direction", None)
 stack_version_unformatted = str(config['hostLevelParams']['stack_version'])
+
+stack_version_unformatted = config['hostLevelParams']['stack_version']
+stack_version_formatted = format_stack_version(stack_version_unformatted)
+
 
 # New Cluster Stack Version that is defined during the RESTART of a Stack Upgrade
 version = default("/commandParams/version", None)
