5b0b362771dc3f66245191e897bcb0d148d838bf
kuhella <35924950+kuhella@users.noreply.github.com>
2018-05-07 22:30:38 +0300

    Update kafka_manager.py

--- ambari-server/src/main/resources/stacks/ADH/1.5/services/KAFKA/package/scripts/kafka_manager.py
+++ ambari-server/src/main/resources/stacks/ADH/1.5/services/KAFKA/package/scripts/kafka_manager.py
@@ -13,7 +13,7 @@ class kafka_manager(Script):
         mode=0755,
         content = InlineTemplate(params.kafka_manager_env_content)
         )
-        File(format("{kafka_manager_conf_dir}/aplication.conf"),
+        File(format("{kafka_manager_conf_dir}/application.conf"),
         mode=0755,
         content = InlineTemplate(params.kafka_manager_application_conf_content)
         )
