41a89b67d3cbd85220b3c91c6171423c214a1d5a
Alexander Ermakov <ean@arenadata.io>
2018-05-24 18:56:21 +0300

    ADH-459 Fixed Catalog Upgrade Script

--- ambari-server/src/main/resources/upgrade/ddl/Ambari-DDL-Postgres-UPGRADE-2.5.sql
+++ ambari-server/src/main/resources/upgrade/ddl/Ambari-DDL-Postgres-UPGRADE-2.5.sql
@@ -1,4 +1,14 @@
-SET search_path=ambari;
-IF EXISTS (SELECT * FROM pg_table WHERE tablename=repo_version) THEN
-    UPDATE cluster_version SET state='CURRENT' where repo_version_id=(SELECT repo_version_id FROM repo_version;
-END IF;
+DO $$
+    BEGIN
+        IF EXISTS
+            ( SELECT 1
+              FROM   information_schema.tables
+              WHERE  table_schema = 'ambari'
+              AND    table_name = 'repo_version'
+            )
+        THEN
+                UPDATE ambari.cluster_version SET state='CURRENT'
+                where repo_version_id=(SELECT repo_version_id FROM ambari.repo_version);
+        END IF ;
+    END
+   $$ ;
