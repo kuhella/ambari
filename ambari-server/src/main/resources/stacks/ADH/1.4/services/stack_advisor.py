cc68e8adae8acd617cbc1dbdc4c7dd6cad34cdff
Alexander Ermakov <ean@arenadata.io>
2018-11-29 17:59:49 +0300

    ADH-712 Fixed KMS Version & Stack Advisor for Ambari Infra

--- ambari-server/src/main/resources/stacks/ADH/1.4/services/stack_advisor.py
+++ ambari-server/src/main/resources/stacks/ADH/1.4/services/stack_advisor.py
@@ -614,7 +614,7 @@ class ADH14StackAdvisor(ADH13StackAdvisor):
 
       solr_zookeeper_url = ",".join(zookeeper_host_arr)
 
-      putAtlasApplicationProperty('atlas.graph.index.search.solr.zookeeper-url', solr_zookeeper_url + "/solr")
+      putAtlasApplicationProperty('atlas.graph.index.search.solr.zookeeper-url', solr_zookeeper_url + "/infra-solr")
     else:
       putAtlasApplicationProperty('atlas.graph.index.search.solr.zookeeper-url', "")
 
